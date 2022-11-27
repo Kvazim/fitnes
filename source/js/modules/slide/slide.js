4323f2ff4200c30f563037b944b19b6307dcabf2
Vladimir <mesvladimir@yandex.ru>
2022-11-27 17:41:48 +0300

    treners

---
+++ source/js/modules/slide/slide.js
@@ -0,0 +1,20 @@
+import Swiper from './vendor/swiper';
+
+const slider = new Swiper('.trainer__wrapper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Default parameters
+  slidesPerView: 4,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.trainer__button--prew',
+    prevEl: '.trainer__button--next',
+  },
+});
+
+slider();
+
+export {slider};
