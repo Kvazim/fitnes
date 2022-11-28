8fd75bb634c99c7a6a30e724cafd82ddbef70be0
Kvazim <mesvladimir@gmail.com>
2022-11-28 17:20:21 +0300

    fix

--- source/js/modules/slide/slide.js
+++ source/js/modules/slide/slide.js
@@ -1,31 +1,36 @@
-/* global swiper, Swiper */
+/* global Swiper */
 /* eslint no-undef: "error" */
+/* eslint no-unused-vars: ["error", { "vars": "local" }] */
+/* global swiper */
 
-const scrollSlider = () => {
-
-  const swiper = new Swiper('.trainer__wrapper', {
-    direction: 'horizontal',
-    loop: true,
-    allowTouchMove: true,
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 30,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
-      1200: {
-        slidesPerView: 4,
-        spaceBetween: 40,
-      },
-    },
-    navigation: {
-      nextEl: '.trainer__button--prev',
-      prevEl: '.trainer__button--next',
-    },
-  });
+const scrollSlider = (item) => {
+  return item;
 };
 
-export { scrollSlider };
+const swiper = new Swiper('.trainer__wrapper', {
+  direction: 'horizontal',
+  loop: false,
+  allowTouchMove: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+  navigation: {
+    nextEl: '.trainer__button--next',
+    prevEl: '.trainer__button--prev',
+  },
+});
+
+scrollSlider(swiper);
+
+// export { scrollSlider };
