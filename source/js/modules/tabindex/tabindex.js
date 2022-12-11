3b25a9a70b7326d38cce847851d108435f4d73e4
Vladimir <mesvladimir@yandex.ru>
2022-12-12 00:03:25 +0300

    fix stylelint

--- source/js/modules/tabindex/tabindex.js
+++ source/js/modules/tabindex/tabindex.js
@@ -1,10 +1,11 @@
 const removeTabIndex = (items) => {
-    const regex = new RegExp(/\./);
-    if (regex.test(items)) {
-        document.querySelectorAll(items).forEach((item) => item.removeAttribute('tabindex'));
-    } else {
-        document.querySelectorAll(items.replace('', '.')).forEach((item) => item.removeAttribute('tabindex'));
-    }
-}
+  const regex = new RegExp(/\./);
+  if (regex.test(items)) {
+    document.querySelectorAll(items).forEach((item) => item.removeAttribute('tabindex'));
+  } else {
+    document.querySelectorAll(items.replace('', '.')).forEach((item) => item.removeAttribute('tabindex'));
+  }
+};
+
 
 export {removeTabIndex};
